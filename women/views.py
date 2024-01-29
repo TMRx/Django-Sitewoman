ab6b91696cb39946a3756bb6f4cec2b0a6b9b810
Asuna <its.a.future.20xx@gmail.com>
2024-01-29 16:34:08 +0200

    configure 'women' application

--- women/views.py
+++ women/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse('Сторінка додатку Women')
+
+def categories(request):
+    return HttpResponse('окремі категорії')
