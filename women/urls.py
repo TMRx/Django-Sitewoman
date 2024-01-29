0d836feb48a97ca152be8b22e61fc45b698ef732
Asuna <its.a.future.20xx@gmail.com>
2024-01-29 18:19:12 +0200

    Add dynamical urls and converter

--- women/urls.py
+++ women/urls.py
@@ -1,8 +1,13 @@
 from . import  views
-from django.urls import path
+from django.urls import path, register_converter
+from . import  converters
+
+register_converter(converters.FourDigitYearConverter, 'year4')
 
 
 urlpatterns = [
     path('', views.index),
-    path('women/', views.categories),
+    path('cats/<int:cat_id>/', views.categories),
+    path('cats/<slug:cat_slug>/', views.categories_by_slug),
+    path('archive/<year4: year>/', views.archive)
 ]
--- women/views.py
+++ women/views.py
@@ -6,5 +6,12 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse('Сторінка додатку Women')
 
-def categories(request):
-    return HttpResponse('окремі категорії')
+def categories(request, cat_id):
+    return HttpResponse(f'<h1> Статті по  категоріям </h1><p>id: {cat_id} </p>')
+
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f'<h1> Статті по  категоріям </h1><p>slug: {cat_slug} </p>')
+
+def archive(request, year):
+    return HttpResponse(f'<h1> Архів по рокам  </h1><p>{year} </p>')
+
