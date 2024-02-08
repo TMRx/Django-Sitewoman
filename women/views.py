c6a0db4641f63c6c73830ecaf20821225945183d
Asuna <its.a.future.20xx@gmail.com>
2024-02-08 15:26:52 +0200

    add redirect and 'reverse' func

--- women/views.py
+++ women/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound
 from django.urls import reverse
 
-
 # Create your views here.
 
 def index(request):
